bf8d674b446a74fcc62a79f1790bc4672e64c658
davidhuertado <davidhuertado@gmail.com>
2021-03-28 16:56:02 -0300

    Agregado elementos de estilos, titulo. Arreglada funcion hoverGrilla y resetGrilla

--- index.js
+++ index.js
@@ -1,28 +1,39 @@
-//create granDiv
+
 
 const body = document.querySelector("body");
 const granDiv = document.createElement("div");
 const botonReset = document.createElement("button");
-body.appendChild(botonReset);
-body.appendChild(granDiv);
+const abarcadorDiv = document.createElement("div");
+const titulo = document.createElement("h1");
+
+body.appendChild(titulo);
+body.appendChild(abarcadorDiv);
+abarcadorDiv.appendChild(botonReset);
+abarcadorDiv.appendChild(granDiv);
 
 let grilla = crearGrilla();
 
-body.style.display = "flex";
-body.style.alignItems = "center";
-body.style.justifyContent = "center";
+body.style.textAlign = "center";
+body.style.backgroundColor = "#864000";
+
+
+titulo.innerText = "Etch-a-Sketch";
+titulo.style.color = "#ffefcf";
+titulo.style.padding = "20px";
+
+abarcadorDiv.style.height = "600px"
+abarcadorDiv.style.display = "flex";
+abarcadorDiv.style.alignItems = "center";
+abarcadorDiv.style.justifyContent = "center";
 
 granDiv.style.display = "grid";
 granDiv.style.width = "500px";
 granDiv.style.height = "500px";
 granDiv.style.margin = "6rem";
-granDiv.style.backgroundColor = "#fff";
+granDiv.style.backgroundColor = "#d44000";
 granDiv.style.borderStyle = "solid";
-granDiv.style.borderColor = "yellow";
+granDiv.style.borderColor = "#ff7a00";
 granDiv.setAttribute("id", "gran-div");
-
-
-
 granDiv.style.gridTemplateColumns = "repeat(16, 1fr)";
 granDiv.style.gridTemplateRows = "repeat(16, 1fr)";
 
@@ -33,43 +44,55 @@ botonReset.addEventListener("click", function () {
 });
 let grillaConHover = hoverGrilla(grilla);
 
-//
 
+//Reset la grilla al numero que desee el usuario
 function resetGrilla(granDiv, crearGrilla, hoverGrilla) {
   let cantidadColumnas = prompt("Size of the square (2 - 100 and integers)");
-  cantidadColumnas = parseInt(cantidadColumnas);
-  if (cantidadColumnas < 2 || cantidadColumnas > 100) {
+  if (cantidadColumnas < 2 || cantidadColumnas > 100 || cantidadColumnas % 1 !== 0) {
     alert("ERROR");
     return;
-  } else {
-    let viejaGrilla = document.querySelectorAll(".cadaCuadro");
+  }
+  cantidadColumnas = parseInt(cantidadColumnas);
+  
+   
 
     while (granDiv.firstChild) {
       granDiv.removeChild(granDiv.firstChild);
     }
     let nuevaGrilla = crearGrilla(cantidadColumnas);
-    
     let nuevasColumnas = columnasGranDiv(cantidadColumnas);
-    nuevaGrilla = hoverGrilla(nuevaGrilla);
+    nuevaGrilla = hoverGrilla(nuevaGrilla, cantidadColumnas);
     return nuevasColumnas, nuevaGrilla;
-    
-    // return nuevaGrilla;
-  }
 
+    
+  
+    //Organizar el grid
   function columnasGranDiv(cantidadColumnas) {
     granDiv.style.gridTemplateColumns = "repeat(" + cantidadColumnas + ", 1fr)";
-  granDiv.style.gridTemplateRows = "repeat(" + cantidadColumnas + ", 1fr)";
-  return;
+    granDiv.style.gridTemplateRows = "repeat(" + cantidadColumnas + ", 1fr)";
+    return;
   }
 }
 
-function hoverGrilla(grilla) {
-  for (let i = 0; i < 256; i++) {
-    grilla[i].addEventListener("mouseover", () => {
-      grilla[i].style.backgroundColor = "black";
-    });
+//Hacer hover de la grilla
+function hoverGrilla(grilla, cantidadColumnas) {
+  if (cantidadColumnas == undefined) {
+    for (let i = 0; i < 256; i++) {
+      grilla[i].addEventListener("mouseover", () => {
+        grilla[i].style.backgroundColor = "#ffefcf";
+      });
+    }
+    return;
   }
-  return;
+  else {
+    for (let i = 0; i < cantidadColumnas * cantidadColumnas; i++) {
+      grilla[i].addEventListener("mouseover", () => {
+        grilla[i].style.backgroundColor = "#ffefcf";
+      });
+
+    }
+    return;
+  }
 }
 
 //Crear grilla 16*16
